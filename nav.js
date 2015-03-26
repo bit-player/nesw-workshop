e0bcfbcb6ef02b0bd86c37da103e7e750ccd1d92
Brian Hayes <brian@bit-player.org>
2015-03-26 17:54:22 -0400

    now add .. links in nav.js

--- nav.js
+++ nav.js
@@ -2,14 +2,14 @@
   window.onload = function() {
     var menu = 
         [
-          ["Home", "/index.html"],
-          ["Schedule", "/schedule/schedule.html"],
-          ["Instructions", "/instructions/instructions.html"],
-          ["Examples", "/examples/examples.html"],
-          ["Resources", "/resources/resources.html"],
-          ["Python", "/python/python.html"],
-          ["JavaScript", "/javascript/javascript.html"],
-          ["Contact", "/contact/contact.html"]
+          ["Home", "../index.html"],
+          ["Schedule", "../schedule/schedule.html"],
+          ["Instructions", "../instructions/instructions.html"],
+          ["Examples", "../examples/examples.html"],
+          ["Resources", "../resources/resources.html"],
+          ["Python", "../python/python.html"],
+          ["JavaScript", "../javascript/javascript.html"],
+          ["Contact", "../contact/contact.html"]
         ]
     var navList = document.getElementById("nav");
     for (var i = 0; i < menu.length; i++) {
